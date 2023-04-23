09b39342496432ad0fa3fc9ad1c80d7c9c630c0b
Lornavav <>
2023-04-23 14:18:49 +0000

    Add file to hold questions and populate with questions, options & answers

---
+++ questions.py
@@ -0,0 +1,64 @@
+questions = [
+    {
+        "question": "In which continent are Chile, Argentina and Brazil?",
+        "options": ["North America", "South America", "Europe", "Asia"],
+        "answer": "South America"
+    },
+    {
+        "question": """Which brand of soup featured
+                       in one of Andy Warhols most famous
+                       pop art pieces?""",
+        "options": ["Heinz", "Campbells", "Baxters", "Knorr"],
+        "answer": "Campbells"
+    },
+    {
+        "question": """The Mad Hatter and the Cheshire Cat
+                       are characters in which famous book?""",
+        "options": ["Winne-the-Pooh", "Charlottes Web",
+                    "Charlie and the Chocolate Factory",
+                    "Alice in Wonderland"],
+        "answer": "Alice in Wonderland"
+    },
+    {
+        "question": "What measurement scale is used to determine wind speed?",
+        "options": ["Beaufort scale", "Richter scale",
+                    "Synoptic scale", "Gusting scale"],
+        "answer": "Beaufort scale"
+    },
+    {
+        "question": "How many sides does a dodecahedron have?",
+        "options": ["2", "24", "14", "20"],
+        "answer": "2"
+    },
+    {
+        "question": """The Statue of Liberty was a gift to the United States
+                       from which European country?""",
+        "options": ["Belgium", "Germany", "Spain", "France"],
+        "answer": "France"
+    },
+    {
+        "question": "What is the worlds fastest species of bird?",
+        "options": ["Golden eagle", "Peregrine falcon",
+                    "Frigatebird", "Penguin"],
+        "answer": "Peregrine falcon"
+    },
+    {
+        "question": """Which animated Disney movie features characters
+                       including Rafiki and Zazu?""",
+        "options": ["The Jungle Book", "The Lion King", "Finding Nemo",
+                    "Snow White and the Seven Dwarfs"],
+        "answer": "The Lion King"
+    },
+    {
+        "question": "Which female holds the most Wimbledon titles?",
+        "options": ["Martina Navratilova", "Serena Williams",
+                    "Venus Williams", "Martina Hingis"],
+        "answer": "Martina Navratilova"
+    },
+    {
+        "question": """Which Danish-Norwegian band released the hit song
+                       Barbie Girl in 1997?""",
+        "options": ["ABBA", "Aqua", "Vanilla Ice", "Bjork"],
+        "answer": "Aqua"
+    },
+]
