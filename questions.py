06d4c8488fa3ee3f7819d541c5fb6d5cd9930ece
Lornavav <>
2023-04-24 12:53:07 +0000

    Add new line notation to clean up terminal

--- questions.py
+++ questions.py
@@ -5,13 +5,13 @@ questions = [
         "answer": "South America"
     },
     {
-        "question": """Which brand of soup featured in one of Andy Warhols
+        "question": """\nWhich brand of soup featured in one of Andy Warhols
 most famous pop art pieces?\n""",
         "options": ["Heinz", "Campbells", "Baxters", "Knorr"],
         "answer": "Campbells"
     },
     {
-        "question": """The Mad Hatter and the Cheshire Cat are characters
+        "question": """\nThe Mad Hatter and the Cheshire Cat are characters
 in which famous book?\n""",
         "options": ["Winne-the-Pooh", "Charlottes Web",
                     "Charlie and the Chocolate Factory",
@@ -19,44 +19,44 @@ in which famous book?\n""",
         "answer": "Alice in Wonderland"
     },
     {
-        "question": """What measurement scale is used to determine
+        "question": """\nWhat measurement scale is used to determine
 wind speed?\n""",
         "options": ["Beaufort scale", "Richter scale",
                     "Synoptic scale", "Gusting scale"],
         "answer": "Beaufort scale"
     },
     {
-        "question": "How many sides does a dodecahedron have?\n",
-        "options": ["2", "24", "14", "20"],
+        "question": "\nHow many sides does a dodecahedron have?\n",
+        "options": ["12", "24", "14", "20"],
         "answer": "2"
     },
     {
-        "question": """The Statue of Liberty was a gift to the United States
+        "question": """\nThe Statue of Liberty was a gift to the United States
 from which European country?\n""",
         "options": ["Belgium", "Germany", "Spain", "France"],
         "answer": "France"
     },
     {
-        "question": "What is the worlds fastest species of bird?\n",
+        "question": "\nWhat is the worlds fastest species of bird?\n",
         "options": ["Golden eagle", "Peregrine falcon",
                     "Frigatebird", "Penguin"],
         "answer": "Peregrine falcon"
     },
     {
-        "question": """Which animated Disney movie features characters
+        "question": """\nWhich animated Disney movie features characters
 including Rafiki and Zazu?\n""",
         "options": ["The Jungle Book", "The Lion King", "Finding Nemo",
                     "Snow White and the Seven Dwarfs"],
         "answer": "The Lion King"
     },
     {
-        "question": "Which female holds the most Wimbledon titles?\n",
+        "question": "\nWhich female holds the most Wimbledon titles?\n",
         "options": ["Martina Navratilova", "Serena Williams",
                     "Venus Williams", "Martina Hingis"],
         "answer": "Martina Navratilova"
     },
     {
-        "question": """Which Danish-Norwegian band released the hit song
+        "question": """\nWhich Danish-Norwegian band released the hit song
 Barbie Girl in 1997?\n""",
         "options": ["ABBA", "Aqua", "Vanilla Ice", "Bjork"],
         "answer": "Aqua"
